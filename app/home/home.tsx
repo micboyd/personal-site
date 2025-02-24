c310f0d45bb8dd25ed7245f4137d9aa5006bd8ab
Michael Boyd <michaelb@forrit.com>
2025-02-24 14:18:09 +0000

    Formatting and project admin

---
+++ app/home/home.tsx
@@ -0,0 +1,11 @@
+import Contact from './contact/contact';
+import Header from './header/header';
+
+export default function Home() {
+	return (
+		<div>
+			<Header></Header>
+			<Contact></Contact>
+		</div>
+	);
+}
